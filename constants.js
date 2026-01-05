91c0570e205ca5f07cc37b0a81e0062b8b100fdc
Evgenii Bukarin <bukarinevgeni@gmail.com>
2026-01-05 18:16:59 +0200

    Refactor links in index.html to use constants for improved maintainability and update package dependencies

---
+++ constants.js
@@ -0,0 +1,24 @@
+// Contact Information
+export const CONTACTS = {
+  email: 'bukarinevgeni@gmail.com',
+  phone: '+972587847322',
+};
+
+// Social Media Links
+export const SOCIAL_LINKS = {
+  linkedin: 'https://www.linkedin.com/in/eugenebukarin/',
+  github: 'https://github.com/bukarinevg/',
+  telegram: 'https://t.me/eeuggene',
+  whatsapp: 'https://wa.me/972587847322',
+};
+
+// Project Links
+export const PROJECTS = {
+  weather: 'http://evgbukar.com/weather/',
+  chat: 'https://chat-room-chi-seven.vercel.app/',
+  ticTacToe: 'http://evgbukar.com/tic-tac-toe/',
+  bugKiller: 'http://www.evgbukar.com/bug-killer',
+};
+
+// Form Action
+export const FORM_ACTION = 'https://formspree.io/f/meojjzer';
--- index.js
+++ index.js
@@ -3,9 +3,49 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+import { CONTACTS, SOCIAL_LINKS, PROJECTS, FORM_ACTION } from './constants';
 
 
 document.body.classList.remove('hidden');
+
+// Apply constants to links
+function applyLinks() {
+  // Contact links
+  const emailLink = document.querySelector('[data-link="email"]');
+  const phoneLink = document.querySelector('[data-link="phone"]');
+  
+  if (emailLink) emailLink.href = `mailto:${CONTACTS.email}`;
+  if (phoneLink) phoneLink.href = `tel:${CONTACTS.phone}`;
+  
+  // Social links
+  const linkedinLink = document.querySelector('[data-link="linkedin"]');
+  const githubLink = document.querySelector('[data-link="github"]');
+  const telegramLink = document.querySelector('[data-link="telegram"]');
+  const whatsappLink = document.querySelector('[data-link="whatsapp"]');
+  
+  if (linkedinLink) linkedinLink.href = SOCIAL_LINKS.linkedin;
+  if (githubLink) githubLink.href = SOCIAL_LINKS.github;
+  if (telegramLink) telegramLink.href = SOCIAL_LINKS.telegram;
+  if (whatsappLink) whatsappLink.href = SOCIAL_LINKS.whatsapp;
+  
+  // Project links
+  const weatherLink = document.querySelector('[data-link="weather"]');
+  const chatLink = document.querySelector('[data-link="chat"]');
+  const ticTacToeLink = document.querySelector('[data-link="ticTacToe"]');
+  const bugKillerLink = document.querySelector('[data-link="bugKiller"]');
+  
+  if (weatherLink) weatherLink.href = PROJECTS.weather;
+  if (chatLink) chatLink.href = PROJECTS.chat;
+  if (ticTacToeLink) ticTacToeLink.href = PROJECTS.ticTacToe;
+  if (bugKillerLink) bugKillerLink.href = PROJECTS.bugKiller;
+  
+  // Form action
+  const form = document.getElementById("my-form");
+  if (form) form.action = FORM_ACTION;
+}
+
+applyLinks();
+
 let form = document.getElementById("my-form");
 
 
